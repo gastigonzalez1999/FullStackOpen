33c8ff2206783f4e70f6e88052fd11e778e1aae3
Gaston Gonzalez <gastongonzalez2550.gg@gmail.com>
2024-05-14 21:39:03 -0300

    Update old part2 components exercises

---
+++ FSOpenFromScratch/partTwo/phonebook/src/services/person.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const baseUrl = '/api/persons'
+
+const getAll = async () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = async newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = async (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const remove = async id => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, create, update, remove }
